babf28e456d9d69d0eba214b8a9d900cb6529565
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-11 19:32:49 +0700

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+main (){
+char* my_array[7]={"F1B019037","F1B019
+055",
+"F1B019047","F1B019021","F1B019022",
+"F1B019045","F1B019030"};
+cout<<"Cok Indah Purnama Putri \n";
+cout<<"F1B019037 \n";
+cout<<"Kelompok 7 \n";
+	
+	cout<<"nim mahasiswa ke- 1 = "<<my_array[0]<<"\n";
+	cout<<"nim mahasiswa ke- 2 = "<<my_array[1]<<"\n";
+	cout<<"nim mahasiswa ke- 3 = "<<my_array[2]<<"\n";
+	cout<<"nim mahasiswa ke- 4 = "<<my_array[3]<<"\n";
+	cout<<"nim mahasiswa ke- 5 = "<<my_array[4]<<"\n";
+	cout<<"nim mahasiswa ke- 6 = "<<my_array[5]<<"\n";
+	cout<<"nim mahasiswa ke- 7 = "<<my_array[6]<<"\n";
+
+}
