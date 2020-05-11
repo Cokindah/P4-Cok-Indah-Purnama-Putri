288cb64a9bad98ded2be62834ad07feed6e53302
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-11 19:56:26 +0700

    Create Jobsheet 7 C++.cpp
    Mengisi Elemen Array menggunakan perulangan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+		cout<<"Cok Indah Purnama Putri \n";
+cout<<"F1B019037 \n";
+cout<<"Kelompok 7 \n";
+	int x[7],i;
+	for(i=0;i<7;i++)
+	{
+	cout<<"Masukkan NIM ke- "<<i+1<<" : ";
+	cin>>x[i];
+}
+return 0;
+}
