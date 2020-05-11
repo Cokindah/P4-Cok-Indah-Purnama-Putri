504a3dc51d4e74be2fa5e05dd6f0950eaf483cbe
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-11 19:15:34 +0700

    Create Jobsheet 1 C.c
    Membuat program array satu dimensi yang menampilkan NIM masing-masing mahasiswa.

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+{
+char* nim_ray[]={"F1B019037","F1B019055",
+"F1B019047","F1B019021",
+"F1B019022","F1B019045","F1B019030"};
+printf("Cok Indah Purnama Putri \n");
+printf("NIM : F1B019037 \n");
+printf("Kelompok 7 \n");
+	printf("nim mahasiswa ke- 1 = %s\n",nim_ray[0]);
+	printf("nim mahasiswa ke- 2 = %s\n",nim_ray[1]);
+	printf("nim mahasiswa ke- 3 = %s\n",nim_ray[2]);
+	printf("nim mahasiswa ke- 4 = %s\n",nim_ray[3]);
+	printf("nim mahasiswa ke- 5 = %s\n",nim_ray[4]);
+	printf("nim mahasiswa ke- 6 = %s\n",nim_ray[5]);
+	printf("nim mahasiswa ke- 7 = %s\n",nim_ray[6]);
+	
+}
