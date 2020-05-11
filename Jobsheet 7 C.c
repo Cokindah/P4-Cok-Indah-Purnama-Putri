e2c639e3a576ffe4b2784b73f452fa165e57f4fc
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-11 19:26:39 +0700

    Create Jobsheet 7 C.c
    Mengisi Elemen Array menggunakan perulangan

---
+++ Jobsheet 7 C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+	printf("Cok Indah Purnama Putri \n");
+printf("NIM : F1B019037 \n");
+printf("Kelompok 7 \n");
+	int x[7],i;
+	for(i=0;i<7;i++)
+{
+	printf("Masukkan nilai %i : ",i+1) ;
+	scanf("%i",&x[i]) ;
+	}
+	return 0;
