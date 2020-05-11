a324e3c5fdaf1298af3cc890aa628e540f0a74f4
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-11 19:33:59 +0700

    Create Jobsheet 2 C++.cpp
    Mengisi dan Menampilkan Elemen Array (Dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+main()
+{
+		cout<<"Cok Indah Purnama Putri \n";
+cout<<"F1B019037 \n";
+cout<<"Kelompok 7 \n";
+	
+	int x [7];
+	cout<<"Masukkan nilai 1 : ";
+	cin>>x[0];
+	cout<<"Masukkan nilai 2 : ";
+	cin>>x[1];
+	cout<<"Masukkan nilai 3 : ";
+	cin>>x[2];
+	cout<<"Masukkan nilai 4 : ";
+	cin>>x[3];
+	cout<<"Masukkan nilai 5 : ";
+	cin>>x[4];
+	cout<<"Masukkan nilai 6 : ";
+	cin>>x[5];
+	cout<<"Masukkan nilai 7 : ";
+	cin>>x[6];
+	cout<<"\n";
+	cout<<"Nilai ke-1 = \n"<<x[0];
+	cout<<"Nilai ke-2 = \n"<<x[1];
+	cout<<"Nilai ke-3 = \n"<<x[2];
+	cout<<"Nilai ke-4 = \n”<<x[3];
+	cout<<"Nilai ke-5 = \n"<<x[4];
+	cout<<"Nilai ke-6 = \n"<<x[5];
+	cout<<"Nilai ke-7 = \n"<<x[6];
+	return 0;
+	
+}
