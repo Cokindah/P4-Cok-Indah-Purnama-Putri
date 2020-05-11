fe172e268cd7095ede6dc2bf37e2b73f430d0d2c
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-11 19:29:03 +0700

    Create Jobsheet 9 C.c
    Membuat Array sebagai karakter

---
+++ Jobsheet 9 C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	printf("Cok Indah Purnama Putri \n");
+printf("NIM : F1B019037 \n");
+printf("Kelompok 7 \n");
+	int i;
+	char nama[10]={'c','o','k','i','n','d','a','h'};
+	char nama2[10]="cokindah";
+	printf("Nama per karakter = ");
+	for(i=0;i<10;i++)
+	{
+		printf("%c,",nama[i]);
+		
+	}
+	printf("\n");
+	printf("Nama = %s\n\n",nama2);
+	return 0;
+}
