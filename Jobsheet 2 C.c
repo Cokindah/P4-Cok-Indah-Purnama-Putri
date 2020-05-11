60f817028b27d595443ee4ecc8b794263f5c4400
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-11 19:20:01 +0700

    Create Jobsheet 2 C.c
    Mengisi dan Menampilkan Elemen Array (Dinamis)

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+main()
+{
+	printf("Cok Indah Purnama Putri \n");
+printf("NIM : F1B019037 \n");
+printf("Kelompok 7 \n");
+	int x [7];
+	printf("Masukkan nilai 1 : ");
+	scanf("%i",&x[0]);
+	printf("Masukkan nilai 2 : ");
+	scanf("%i",&x[1]);
+	printf("Masukkan nilai 3 : ");
+	scanf("%i",&x[2]);
+	printf("Masukkan nilai 4 : ");
+	scanf("%i",&x[3]);
+	printf("Masukkan nilai 5 : ");
+	scanf("%i",&x[4]);
+	printf("Masukkan nilai 6 : ");
+	scanf("%i",&x[5]);
+	printf("Masukkan nilai 7 : ");
+	scanf("%i",&x[6]);
+	printf("\n");
+	printf("Nilai ke-1 = %i\n",x[0]);
+	printf("Nilai ke-2 = %i\n",x[1]);
+	printf("Nilai ke-3 = %i\n",x[2]);
+	printf("Nilai ke-4 = %i\n",x[3]);
+	printf("Nilai ke-5 = %i\n",x[4]);
+	printf("Nilai ke-6 = %i\n",x[5]);
+	printf("Nilai ke-7 = %i\n",x[6]);
+	return 0;
+	
+}
