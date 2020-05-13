dabeaa0407a2f960b4d15f637b3f83fd61dc5d01
Cok Indah Purnama Putri <64572302+Cokindah@users.noreply.github.com>
2020-05-13 07:26:59 +0700

    Create Tugas 1.cpp
    Membuat kuis sebanyak 10 soal mengenai COVID-19 dengan menggunakan array

---
+++ Tugas 1.cpp
@@ -0,0 +1,404 @@
+#include<iostream>
+#include<string>
+#include <iomanip>
+#include <conio.h>
+#include <istream>
+using namespace std;
+void soal ()
+{
+	char jawab[50];
+	int i,poin=0;
+	int total;
+	cout<<"============================================================================\n";
+	cout<<"----------------------------TATA CARA PENGERJAAN----------------------------\n";
+	cout<<"============================================================================\n";
+	cout<<"1. Kerjakan soal secara berurutan\n";
+	cout<<"----------------------------------------------------------------------------\n";
+	cout<<"2. Kerjakan dengan jujur\n";
+	cout<<"----------------------------------------------------------------------------\n";
+	cout<<"3. Masukkan pilihan jawaban dengan menggunakan huruf kapital\n";
+	cout<<"----------------------------------------------------------------------------\n";
+	cout<<"                           SELAMAT MENGERJAKAN \n";
+	cout<<"============================================================================\n";
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+    cout<<"1. Virus Corona (COVID-19) yang menyerang manusia muncul di negara....pada awal\n";
+    cout<<"   tahun 2020 \n";
+	cout<<"================================================================="<<endl;
+	cout<<"     Pilihan Jawaban			                                    "<<endl;
+	cout<<"================================================================="<<endl;
+	cout<<"|| a. Cina                                                      ||\n";            
+	cout<<"|| b. Italia                                                    ||\n";           
+	cout<<"|| c. Indonesia                                                 ||\n";
+	cout<<"|| d. Amerika                                                   ||\n";
+	cout<<"=================================================================\n"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"================================================================="<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    poin+10;
+  } else if (jawab[i]=='B') {
+   poin-5;
+  } else if (jawab[i]=='C') {
+   poin-5;
+  } else if (jawab[i]=='D') {
+   poin-5;
+  } else {
+   poin= 0;
+  }
+  }
+  		
+	cout<<"2. Berdasarkan hasil penelitian dari National Institute of Allergy and Infectious \n";
+	cout<<"   Diseases, ukuran COVID-19 adalah.... \n";
+	cout<<"================================================================="<<endl;
+	cout<<"      Pilihan Jawaban		    	                            "<<endl;
+	cout<<"================================================================="<<endl;
+	cout<<"||a. 120-160 nanometer                                         ||\n";            
+	cout<<"||b. 140-180 nanometer                                         ||\n";           
+	cout<<"||c. 160-200 nanometer                                         ||\n";
+	cout<<"||d. 200-300 nanometer                                         ||\n";   
+	cout<<"================================================================="<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"================================================================="<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin+10;
+  	} else if (jawab[i]=='B') {
+  	 poin -5;
+  	} else if (jawab[i]=='C') {
+   	poin -5;
+  	} else if (jawab[i]=='D') {
+  	 poin -5;
+  	} else {
+   	poin= 0;
+  	}
+  	}
+	
+	cout<<"3. COVID-19 bisa masuk melalui anggota-anggota tubuh di bawah ini, kecuali.... \n";
+	cout<<"===================================================================="<<endl;
+	cout<<"     Pilihan Jawaban		                                           "<<endl;
+	cout<<"===================================================================="<<endl;
+	cout<<"||a. Mata                                                         ||\n";           
+	cout<<"||b. Hidung                                                       ||\n";           
+	cout<<"||c. Mulut                                                        ||\n";
+	cout<<"||d. Telinga                                                      ||\n";            
+	cout<<"===================================================================="<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"===================================================================="<<endl;
+    cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin-5;
+  	} else if (jawab[i]=='C') {
+   	poin-5;
+  	} else if (jawab[i]=='D') {
+  	 poin +10;
+  	} else {
+   	poin= 0;
+  	}
+  	}
+     
+    cout<<"4. Upaya psikologis agar daya tahan tubuh kuat untuk menjaga agar tidak tertular \n";
+	cout<<"   COVID-19 adalah.... \n";
+	cout<<"====================================================================="<<endl;
+	cout<<"                Pilihan Jawaban		    	                        "<<endl;
+	cout<<"====================================================================="<<endl;
+	cout<<"|| a. Tidak panik, tidak stress, dan tetap waspada                 ||\n";            
+	cout<<"|| b. Banyak berdoa, optimis, dan pantang menyerah                 ||\n";           
+	cout<<"|| c. Banyak hiburan, bersuka cita, dan bersenang-senang           ||\n";
+	cout<<"|| d. Banyak berolahraga, bernyanyi dan humor                      ||\n";   
+	cout<<"====================================================================="<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"====================================================================="<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin+10;
+  	} else if (jawab[i]=='B') {
+  	 poin -5;
+  	} else if (jawab[i]=='C') {
+   	poin -5;
+  	} else if (jawab[i]=='D') {
+  	 poin -5;
+  	} else {
+   	poin= 0;
+  	}
+  	}
+
+    cout<<"5. Peran serta masyarakat diperlukan untuk mencegah COVID-19 yang semakin menyebar. \n";
+	cout<<"   Hal-hal yang bisa dilakukan dengan cara sebagai berikut, kecuali.... \n";
+	cout<<"========================================================================"<<endl;
+	cout<<"                       Pilihan Jawaban		                           "<<endl;
+	cout<<"========================================================================"<<endl;
+	cout<<"|| a. Menjalankan pola hidup bersih dan sehat                         ||\n";            
+	cout<<"|| b. Tetap diam di rumah sesuai dengan himbauan pemerintah           ||\n";           
+	cout<<"|| c. Banyak makan dan minum                                          ||\n";
+	cout<<"|| d. Ikut menyosialisasikan pencegahan COVID-19 melalui media sosial ||\n";   
+	cout<<"========================================================================"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"========================================================================"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin -5;
+  	} else if (jawab[i]=='C') {
+   	poin +10;
+  	} else if (jawab[i]=='D') {
+  	 poin -5;
+  	} else {
+   	poin= 0;
+  	}
+  	}
+    cout<<"6. Suhu tubuh yang bisa diindikasikan sedang terjangkit penyakit termasuk \n";
+	cout<<"   COVID-19 yaitu.... \n";
+	cout<<"========================================================================"<<endl;
+	cout<<"                       Pilihan Jawaban		                           "<<endl;
+	cout<<"========================================================================"<<endl;
+	cout<<"|| a. 35 derajat celcius                                              ||\n";            
+	cout<<"|| b. 36 derajat celcius                                              ||\n";           
+	cout<<"|| c. 37 derajat celcius                                              ||\n";
+	cout<<"|| d. 38 derajat celcius                                              ||\n";   
+	cout<<"========================================================================"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"========================================================================"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin -5;
+  	} else if (jawab[i]=='C') {
+   	poin -5;
+  	} else if (jawab[i]=='D') {
+  	 poin +10;
+  	} else {
+   	poin= 0;
+  	}
+  	}
+
+	
+	cout<<"6. Masa inkubasi COVID-19 selama..... \n";
+	cout<<"========================================================================"<<endl;
+	cout<<"                      Pilihan Jawaban		                           "<<endl;
+	cout<<"========================================================================"<<endl;
+	cout<<"|| a. 7 hari                                                          ||\n";            
+	cout<<"|| b. 14 hari                                                         ||\n";           
+	cout<<"|| c. 28 hari                                                         ||\n";
+	cout<<"|| d. 32 hari                                                         ||\n";   
+	cout<<"========================================================================"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"========================================================================"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin +10;
+  	} else if (jawab[i]=='C') {
+   	poin -5;
+  	} else if (jawab[i]=='D') {
+  	 poin -5;
+  	} else {
+   	poin= 0;
+  	}
+  	}
+	
+	cout<<"7. Perhatikan aktivitas-aktivitas di bawah ini : \n";
+	cout<<"   1. Mencuci tangan minimal 40-60 detik\n";
+	cout<<"   2. Berjemur di bawah sinar matahari selama 15-30 menit\n";
+	cout<<"   3. Makan makanan dan minuman bergizi\n";
+	cout<<"   4. Istirahat yang cukup\n";
+	cout<<"   5. Memakai masker\n";
+	cout<<"Upaya untuk menjaga diri terhindar dari COVID-19 di rumah yang tepat adalah....";
+	cout<<"========================================================================"<<endl;
+	cout<<"                       Pilihan Jawaban		                           "<<endl;
+	cout<<"========================================================================"<<endl;
+	cout<<"|| a. 1,2,3,4                                                         ||\n";            
+	cout<<"|| b. 2,3,4,5                                                         ||\n";           
+	cout<<"|| c. 1,2,4,5                                                         ||\n";
+	cout<<"|| d. 1,3,4,5                                                         ||\n";   
+	cout<<"========================================================================"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"========================================================================"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin -5;
+  	} else if (jawab[i]=='C') {
+   	poin -5;
+  	} else if (jawab[i]=='D') {
+  	 poin +10;
+  	} else {
+   	poin= 0;
+  	}
+  	}	
+	
+	cout<<"8. Untuk membangun kesadaran masyarakat terhadap bahaya COVID-19, maka hal yang";
+	cout<<"   perlu dilakukan adalah sebagai berikut, kecuali....";
+	cout<<"========================================================================"<<endl;
+	cout<<"                      Pilihan Jawaban		                           "<<endl;
+	cout<<"========================================================================"<<endl;
+	cout<<"|| a. Melakukan sosialiasasi dengan bahasa dan media yang mudah       ||\n";            
+	cout<<"||    dipahami                                                        ||\n";
+	cout<<"|| b. Meminta masyarakat untuk membaca sendiri informasi seputar      ||\n";           
+	cout<<"||    COVID-19                                                        ||\n";
+	cout<<"|| c. Diberikan kumpulan berita yang isinya tentang daftar korban     ||\n";
+	cout<<"||    COVID-19                                                        ||\n";
+	cout<<"|| d. Diberikan kumpulan doa untuk terhindar dari wabah COVID-19      ||\n";   
+	cout<<"========================================================================"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"========================================================================"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin -5;
+  	} else if (jawab[i]=='C') {
+   	poin -5;
+  	} else if (jawab[i]=='D') {
+  	 poin +10;
+  	} else {
+   	poin= 0;
+  	}
+  	}	
+	
+	cout<<"9. Jenis vitamin yang disarankan dikonsumsi untuk meningkatkan daya tahan";
+	cout<<"   tubuh agar terhindar dari COVID-19 adalah....";
+	cout<<"========================================================================"<<endl;
+	cout<<"                       Pilihan Jawaban		                           "<<endl;
+	cout<<"========================================================================"<<endl;
+	cout<<"|| a. Vitamin A, B dan D                                              ||\n";            
+	cout<<"|| b. Vitamin B, C dan E                                              ||\n";           
+	cout<<"|| c. Vitamin C                                                       ||\n";
+	cout<<"|| d. Vitamin D                                                       ||\n";   
+	cout<<"========================================================================"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"========================================================================"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin +10;
+  	} else if (jawab[i]=='C') {
+   	poin -5;
+  	} else if (jawab[i]=='D') {
+  	 poin -5;
+  	} else {
+   	poin= 0;
+  	}
+  	}
+	
+	cout<<"10. Tujuan menjaga jarak (Social distancing/Physical distancing) sebagaimana";
+	cout<<"    yang diintruksikan oleh pemerintah kepada masyarakat yaitu....";
+	cout<<"========================================================================"<<endl;
+	cout<<"                       Pilihan Jawaban		                           "<<endl;
+	cout<<"========================================================================"<<endl;
+	cout<<"|| a. Agar orang-orang tidak terlalu akrab antara satu sama lain      ||\n";            
+	cout<<"|| b. Membudayakan antri dan disiplin                                 ||\n";           
+	cout<<"|| c. Mengantisipasi penyebaran COVID-19                              ||\n";
+	cout<<"|| d. Supaya orang-orang tidak berdesakan di tempat umum              ||\n";   
+	cout<<"========================================================================"<<endl; 
+	cout<<"Jawaban Anda : ";cin>>jawab;
+	cout<<"========================================================================"<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<endl;
+	{if(jawab[i]=='A'){
+    	poin -5;
+  	} else if (jawab[i]=='B') {
+  	 poin -5;
+  	} else if (jawab[i]=='C') {
+   	poin +10;
+  	} else if (jawab[i]=='D') {
+  	 poin -5;
+  	} else {
+   	poin= 0;
+  	}
+  	}	
+	
+	
+	
+
+	cout<<"==============================================================================="<<endl;
+	cout<<"                           HASIL PEROLEHAN NILAI"<<endl;
+	cout<<"==============================================================================="<<endl;
+	total=poin*10;
+	cout<<"Nilai Anda adalah : "<<total<<endl;
+	
+}
+int main ()
+{
+	
+
+	char kembali,selesai;
+	char nama[50];
+	char nim[10],prodi[50],universitas[20];
+	int pilih;
+	
+	atas:
+	cout<<"=============================================================================="<<endl;
+	cout<<"-------------------------------COVID-19---------------------------------------"<<endl;
+	cout<<"=============================================================================="<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"********************\n";
+	cout<<"ISI BIODATA ANDA\n";
+	cout<<"********************\n";
+	cout<<"Nama        :";cin.getline(nama,50);
+	cout<<"NIM         :";cin.getline(nim,10);
+	cout<<"Prodi       :";cin.getline(prodi,50);
+	cout<<"Universitas :";cin.getline(universitas,20);
+	cout<<endl;
+	cout<<"1. Lanjutkan ke halaman pertanyaan\n";
+	cout<<"2. Selesai\n";
+	cout<<"Masukkan pilihan Anda :";
+	cin>>pilih;
+	switch(pilih)
+	{
+		case (1) : 
+		{       system("cls");
+			    soal();
+				cout<<"Apakah anda ingin mencoba lagi? (Y/T) : "; cin>>kembali;
+				if (kembali=='y' || kembali=='Y')
+				{system("cls"); goto atas;}
+				if (selesai=='t' || selesai=='T')
+				{goto bawah;}
+		}
+		break;
+		case (2) :
+		{
+			system("cls");
+			goto bawah;
+		}
+	bawah:
+	cout<<"*****************************************************\n";
+	cout<<"*****************************************************\n";
+	cout<<"*TERIMA KASIH KARENA SUDAH MENGERJAKAN DENGAN JUJUR** \n";
+	cout<<"*****************************************************\n";
+	cout<<"*****************************************************\n";
+	return 0;
+}
+}
